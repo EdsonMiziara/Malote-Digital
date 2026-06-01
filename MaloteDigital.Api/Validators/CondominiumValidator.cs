30cdbceb8902499e907987f90a580885a36eb831
EdsonMiziara <edselmizi@gmail.com>
2026-06-01 15:45:33 -0300

    feat: implement postgres infrastructure and condominium endpoints

---
+++ MaloteDigital.Api/Validators/CondominiumValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using MaloteDigital.Domain.Entities;
+
+namespace MaloteDigital.Api.Validators;
+
+public class CondominiumValidator : AbstractValidator<Condominium>
+{
+    public CondominiumValidator()
+    {
+        RuleFor(x => x.PreferredPaymentDate)
+            .NotEmpty()
+            .InclusiveBetween(1, 31);
+    }
+
+}
---
+++ MaloteDigital.Domain/entities/Condominium.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MaloteDigital.Domain.Entities;
+
+public class Condominium
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int PreferredPaymentDate { get; set; }
+    public ICollection<Expense> Expenses {  get; set; } 
+}
